3ac48ff345bba4d9c0ff0923b24276c9287065fd
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-05-04 12:11:58 +0300

    +

---
+++ src/main/java/pro/sky/java/test/accountingautomation/AccountingAutomationApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.test.accountingautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/test/accountingautomation/AccountingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.test.accountingautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
